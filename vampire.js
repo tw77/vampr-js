3411c817bfe5ebed9e5fac7710e47163509016eb
Thomas Weideman <thomasweideman@Thomass-MBP.hitronhub.home>
2021-02-21 12:24:05 -0800

    Add methods from first assignment

--- vampire.js
+++ vampire.js
@@ -10,35 +10,31 @@ class Vampire {
 
   // Adds the vampire as an offspring of this vampire
   addOffspring(vampire) {
-
+    this.offspring.push(vampire);
+    vampire.creator = this;
   }
 
   // Returns the total number of vampires created by that vampire
   get numberOfOffspring() {
-
+    return this.offspring.length;
   }
 
   // Returns the number of vampires away from the original vampire this vampire is
   get numberOfVampiresFromOriginal() {
+    let numberOfVampiresFromOriginal = 0;
+    let vampire = this;
 
+    while (vampire.creator) {
+      vampire = vampire.creator;
+      numberOfVampiresFromOriginal++;
+    } 
+    return numberOfVampiresFromOriginal;
   }
 
   // Returns true if this vampire is more senior than the other vampire. (Who is closer to the original vampire)
   isMoreSeniorThan(vampire) {
-
-  }
-
-  /** Stretch **/
-
-  // Returns the closest common ancestor of two vampires.
-  // The closest common anscestor should be the more senior vampire if a direct ancestor is used.
-  // For example:
-  // * when comparing Ansel and Sarah, Ansel is the closest common anscestor.
-  // * when comparing Ansel and Andrew, Ansel is the closest common anscestor.
-  closestCommonAncestor(vampire) {
-
+    return (this.offspring.length > vampire.offspring.length);
   }
 }
 
 module.exports = Vampire;
-
